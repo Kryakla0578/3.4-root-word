7d3a323a27395c57315fe00858998bd5a0a9348e
Kryakla0578 <167692954+Kryakla0578@users.noreply.github.com>
2024-11-19 09:43:51 +0300

    Add files via upload

--- 3.4_root.py
+++ 3.4_root.py
@@ -1,13 +1,12 @@
 def single_root_word(root_word, *other_words):
     same_words = []
-    same_words.append(root_word)
     for i in other_words:
-        if root_word.upper() in i.upper():
+        if root_word.upper() in i.upper() or i.upper() in root_word.upper():
             same_words.append(i)
     return same_words
 
 
 result1 = single_root_word('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 print(result1)
-result2 = single_root_word('Able', 'Disablement', 'Mable', 'Disable', 'Bagel')
+result2 = single_root_word('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 print(result2)
